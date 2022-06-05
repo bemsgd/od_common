3f0b1df27acb29047e19fa7964fec50aace64612
Ruch Phetcharat <ruch.p@bemplc.co.th>
2022-06-05 15:18:31 +0700

    add: conf arg

--- config/config.go
+++ config/config.go
@@ -20,23 +20,22 @@ func SetConfigFileName(fn string) {
 
 func GetYamlAppConfig() *Config {
 	if appConfig == nil {
-		appConfig = loadYamlConfiguration(configFileName)
+		appConfig = loadYamlConfiguration(configFileName, appConfig)
 	}
 	return appConfig
 }
 
-func loadYamlConfiguration(fileName string) *Config {
+func loadYamlConfiguration(fileName string, config *Config) *Config {
 	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln("Error Cannot read app configuraion : ", fileName, err)
 	}
 	defer f.Close()
 
-	var cfg Config
 	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(&cfg)
+	err = decoder.Decode(config)
 	if err != nil {
 		log.Fatalln("Error Cannot initial app configuraion : ", fileName, err)
 	}
-	return &cfg
+	return config
 }
