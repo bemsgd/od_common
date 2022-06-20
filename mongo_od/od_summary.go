4d27d25d1d92a2a878b3d5ab46e873e27a6808f2
Ruch Phetcharat <ruch.p@bemplc.co.th>
2022-06-20 15:20:26 +0700

    add: combine oddetail

--- mongo_od/od_summary.go
+++ mongo_od/od_summary.go
@@ -1,8 +1,53 @@
 package mongo_od
 
+import (
+	"time"
+
+	"github.com/bemsgd/od_common/travelling"
+)
+
 type ODDataSummary struct {
-	TravelDate        string              `json:"TravelDate" bson:"TravelDate"`
-	TravelHour        string              `json:"TravelHour" bson:"TravelHour"`
-	ODMatchingSummary MatchingSummaryData `json:"ODMatchingSummary" bson:"ODMatchingSummary"`
-	ODDetails         []ODDetail          `json:"ODDetails" bson:"ODDetails"`
+	TravelYear             string                     `json:"TravelYear" bson:"TravelYear"`
+	TravelMonth            string                     `json:"TravelMonth" bson:"TravelMonth"`
+	TravelDate             string                     `json:"TravelDate" bson:"TravelDate"`
+	TravelHour             string                     `json:"TravelHour" bson:"TravelHour"`
+	ODMatchingSummary      MatchingSummaryData        `json:"ODMatchingSummary" bson:"ODMatchingSummary"`
+	Origin                 string                     `bson:"Origin" json:"Origin"`
+	Destination            string                     `bson:"Destination" json:"Destination"`
+	VehicleClass           string                     `bson:"VehicleClass" json:"VehicleClass"`
+	PaymentType            string                     `bson:"PaymentType" json:"PaymentType"`
+	MaxTravellingTime      time.Duration              `bson:"MaxTravellingTime" json:"MaxTravellingTime"`
+	MinTravellingTime      time.Duration              `bson:"MinTravellingTime" json:"MinTravellingTime"`
+	TotalTravellingTime    time.Duration              `bson:"TotalTravellingTime" json:"TotalTravellingTime"`
+	TotalTransaction       int                        `bson:"TotalTransaction" json:"TotalTransaction"`
+	MaxTimeTravellingChain travelling.TravellingChain `bson:"MaxTimeTravellingChain" json:"MaxTimeTravellingChain"`
+	MinTimeTravellingChain travelling.TravellingChain `bson:"MinTimeTravellingChain" json:"MinTimeTravellingChain"`
+}
+
+func (odd *ODDataSummary) SetMaxTimeTravellingChain(t travelling.TravellingChain) {
+	odd.MaxTimeTravellingChain = t
+}
+
+func (odd *ODDataSummary) SetMinTimeTravellingChain(t travelling.TravellingChain) {
+	odd.MinTimeTravellingChain = t
+}
+
+func (odd *ODDataSummary) SetMaxTravellingTime(duration time.Duration) {
+	odd.MaxTravellingTime = duration
+}
+
+func (odd *ODDataSummary) SetMinTravellingTime(duration time.Duration) {
+	odd.MinTravellingTime = duration
+}
+
+func (odd *ODDataSummary) UpdateTotalTravellingTime(duration time.Duration) {
+	odd.TotalTravellingTime += duration
+}
+
+func (odd *ODDataSummary) IncTotalTransaction() {
+	odd.TotalTransaction += 1
+}
+
+func (odd *ODDataSummary) GetAvgTravelingTime() time.Duration {
+	return odd.TotalTravellingTime / time.Duration(odd.TotalTransaction)
 }
