23fe7e311941de75b0d15aba902d6ce8f15cad89
Ruch Phetcharat <ruch.p@bemplc.co.th>
2022-07-29 09:50:09 +0700

    add: check holiday date

--- mongo_od/holiday.go
+++ mongo_od/holiday.go
@@ -1,7 +1,26 @@
 package mongo_od
 
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 type Holiday struct {
 	Date        string `bson:"Date" json:"Date"`
 	Description string `bson:"Description" json:"Description"`
 	CreateBy    string `bson:"CreateBy" json:"CreateBy"`
+}
+
+func (adb *MongoRepository) IsHolidayDate(date string) bool {
+	ctx := adb.Context
+	collection := adb.Collection
+	var h Holiday
+	filter := bson.M{"Date": date}
+
+	err := collection.FindOne(ctx, filter).Decode(&h)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return h.Date == ""
 }
